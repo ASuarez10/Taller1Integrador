2a50e9b7f7ec921703adb85157abdfc808ecb13b
Alejandro Suarez <asuarez100501@gmail.com>
2020-08-21 20:06:39 -0500

    Adelanto del gráfico

---
+++ ui/VentanaGrafico.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Taller1Integrador.ui
+{
+    partial class VentanaGrafico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(82, 12);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(657, 461);
+            this.chart2.TabIndex = 0;
+            this.chart2.Text = "chart2";
+            // 
+            // VentanaGrafico
+            // 
+            this.ClientSize = new System.Drawing.Size(870, 485);
+            this.Controls.Add(this.chart2);
+            this.Name = "VentanaGrafico";
+            this.Text = "VentanaGrafico";
+            this.Load += new System.EventHandler(this.VentanaGrafico_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
---
+++ ui/VentanaGrafico.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+namespace Taller1Integrador.ui
+{
+    public partial class VentanaGrafico : Form
+    {
+        public VentanaGrafico()
+        {
+            InitializeComponent();
+        }
+
+        private void VentanaGrafico_Load_1(object sender, EventArgs e)
+        {
+            string[] series = { "Alejandro", "Esteban", "Juan" };
+            int[] puntos = { 20, 50, 70 };
+
+            chart1.Titles.Add("Ejemplo");
+
+            for (int i = 0; i < series.Length; i++)
+            {
+
+                //titulos
+                Series serie = chart1.Series.Add(series[i]);
+
+                //cantidades
+                serie.Label = puntos[i].ToString();
+
+                serie.Points.Add(puntos[i]);
+            }
+        }
+    }
+}
